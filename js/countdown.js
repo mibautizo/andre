c6ddecfccc3d08ba3b78d80df89572e93a51fc66
mibautizo <mibautizo189@gmail.com>
2026-06-29 22:12:03 -0600

    Add files via upload

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ simplyCountdown('#cuenta', {
 	year: 2026, // required
 	month: 8, // required
 	day: 22, // required
-	hours: 8, // Default is 0 [0-23] integer
+	hours: 6, // Default is 0 [0-23] integer
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
